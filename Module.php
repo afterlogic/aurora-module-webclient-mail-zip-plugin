350b38c8c250656b0e48f79626a461743dd4603c
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:00:34 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\MailZipWebclientPlugin;
 
 /**
+ * Adds Expand button on ZIP-attachment and shows its content.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
